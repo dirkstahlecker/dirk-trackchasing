bba9e222f89dc58419b414ee2cb871294bfc3864
Dirk Stahlecker <dirkstahlecker@Dirks-MacBook-Pro.local>
2020-12-12 21:16:25 -0500

    added warning message about not touching finnicky heroku things. Heroku deployment currently works

--- src/server.ts
+++ src/server.ts
@@ -324,27 +324,16 @@ app.get('/stats', async function (req, res) {
 });
 
 
+//Don't touch the following - Heroku gets very finnicky about it
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-// 	const index = path.join(__dirname, '../client/build', 'index.html');
-// 	console.log(index)
-// 	//path.join(__dirname+'/../../client/build/index.html')
-//   res.sendFile(index);
-// });
 
 const root = path.join(__dirname, '..', 'client', 'build')
 app.use(express.static(root));
 app.get("*", (req, res) => {
     res.sendFile('index.html', { root });
 })
-
-
-
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
